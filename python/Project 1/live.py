b01a37168acdc7f720cf01eae43a689cc867f3f4
Home Desktop <david.lanin@gmail.com>
2022-05-27 20:30:10 +0300

    "27.05.2022 update 1"

--- python/Project 1/live.py
+++ python/Project 1/live.py
@@ -1,6 +1,5 @@
 import logging
 import os,imp
-from tkinter import Misc
 
 from misc import clear_screen
 
@@ -54,47 +53,50 @@ def load_game(path,name):
     games=[]
     games = load_library(path)
     try:
-        gamescount = len(games)
+        gamesCount = len(games)
     except:
-        gamescount = 0
-    if gamescount > 0:       
+        gamesCount = 0
+    if gamesCount > 0:       
         wins = 0
         loses = 0
         
         selection = -1
         while not selection == 0:
+            clear_screen()
             #print menu header
             print(f"Curent player <{name}>. Wins:{wins} | Loses:{loses}")
             #print games selection
-            for i in range(gamescount):
+            for i in range(gamesCount):
                 print(f"{i+1}: {games[i]}")
             #print exit prompt
             print("----------------------")
             print("0:Exit")
             try:
-                selection = int(input(f'Your choice {name}:'))
+                selection = int(input(f'Your choice, {name}:'))
                 if selection in games:
                     pass
             except:
                 selection = -1
-            if not (selection == 0) and (selection-1) in range(gamescount):
+            if (selection-1) in range(gamesCount):
+                #run game
                 dificulty = get_dificulty()
-                logging.info(f'Starting game ({games[selection-1]})')
+                logging.info(f'Starting game<{games[selection-1]}> at ({dificulty}) dificulty')
                 games[selection-1].set_dificulty(dificulty)
                 games[selection-1].play(name)
+                logging.info(f"Game<{games[selection-1]}> at ({dificulty}) dificulty ended with <{('win' if games[selection-1].result else 'lose')}> result")
                 if games[selection-1].get_result():
                     wins += 1
                 else:
                     loses += 1
-            else:
-                clear_screen()
     else:
         logging.error("Unable to run. No games exist")
     
     
 def welcome():
     name=input("Hi \n Please enter your name:")
-    print(f"Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play.")
+    print(f"Hello {name} and welcome to the World of Games (WoG).\n\
+        Here you can find many cool games to play.")
+    os.system('pause')
     return name
 
 if __name__ == '__main__':
