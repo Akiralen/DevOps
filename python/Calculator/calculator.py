6443b0bf3486c937d8768974dc590db88c39f07a
Home Desktop <david.lanin@gmail.com>
2022-05-30 23:32:42 +0300

    Final Calc

--- python/Calculator/calculator.py
+++ python/Calculator/calculator.py
@@ -1,19 +1,39 @@
+def get_inner_expression(string : str):
+    inner_string = ""
+    for i, char in enumerate(string):
+        if char == "(" :
+            inner_string = '('
+        elif char == ")":
+            inner_string += ')'
+            break
+        else:
+            inner_string += char
+    string_list = list(string.partition(inner_string))
+    string_list[1] = string_list[1][1:-1]
+    return string_list
+    
+
+
 def calculator (calc_expression : str):
-    calc_expression.replace(" ","")
+    print(calc_expression)
+    if '(' in calc_expression:
+       start,middle,end = get_inner_expression(calc_expression)
+       return calculator(start + str(calculator(middle)) + end)
+        
     if calc_expression.isdigit():
         return int(calc_expression)
-    elif '*' in calc_expression:
-        n1,n2 = calc_expression.split('*',1)
-        return calculator(n1) + calculator(n2)
-    elif '/' in calc_expression:
-        n1,n2 = calc_expression.split('/',1)
-        n2 = calculator(n2)
     elif '-' in calc_expression:
         n1,n2 = calc_expression.split('-',1)
         return calculator(n1) - calculator(n2)
     elif '+' in calc_expression:
         n1,n2 = calc_expression.split('+',1)
+        return (calculator(n1) + calculator(n2))
+    elif '*' in calc_expression:
+        n1,n2 = calc_expression.split('*',1)
         return calculator(n1) * calculator(n2)
+    elif '/' in calc_expression:
+        n1,n2 = calc_expression.split('/',1)
+        n2 = calculator(n2)
         if n2 == 0:
             raise ZeroDivisionError
         else:
@@ -22,9 +42,9 @@ def calculator (calc_expression : str):
         raise RuntimeError("Ilegal expression")
 
 def _main():
-    user_input=input("Please enter expression")
+    user_input=input("Please enter expression: ")
+    user_input.replace(" ","")
     print(calculator(user_input))
-
 if __name__ == "__main__":
     _main()
 else:
